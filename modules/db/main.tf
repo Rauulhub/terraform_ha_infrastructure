1c5fa93faf6acc2543895c2f27de0f25eed627a8
Raul Sebastian Carreño Reyes <63383820+Rauulhub@users.noreply.github.com>
2025-03-27 10:30:22 -0500

    Update main.tf

--- modules/db/main.tf
+++ modules/db/main.tf
@@ -19,7 +19,7 @@ resource "aws_db_instance" "lab_rds" {
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
   username             = "applicationuser"
-  password             = "applicationuser"
+  password             = "<yourpasswd>"
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
   publicly_accessible     = false
