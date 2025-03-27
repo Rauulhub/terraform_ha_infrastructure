f79cbc747c4ce850dc24b4bc74c740356482c2c0
Raul Sebastian Carreño Reyes <63383820+Rauulhub@users.noreply.github.com>
2025-03-27 10:32:11 -0500

    Update null_resource.tf

--- modules/ec2/null_resource.tf
+++ modules/ec2/null_resource.tf
@@ -5,18 +5,18 @@
 #    type     = "ssh"
 #    user     = "ec2-user"
 #    host     = aws_instance.ec2_management.public_ip
-#    private_key = file("${path.module}/key_file.pem")
+#    private_key = file("${path.module}/<key_file>")
 #  }
 #
 #  # File provisioner to push the .pem file to Bastion EC2 instance
 #  provisioner "file" {
-#    source = "private_file.pem"
-#    destination = "/tmp/private_file.pem"
+#    source = "<key_file>"
+#    destination = "/tmp/<key_file>"
 #  }
 #
 #  # Remote-exec is used to provide executable permission via below command
 #  provisioner "remote-exec" {
-#    inline = ["sudo chmod 400 /tmp/private_file.pem"]
+#    inline = ["sudo chmod 400 /tmp/<key_file>"]
 #  }
 #
 #}
